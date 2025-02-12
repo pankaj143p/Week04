6c91af159ca8468a103cb66096c77d7a545c3a49
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-12 15:18:14 +0530

    [Pankaj] : generate binary using queue code added

---
+++ day_02/src/problems/queue/GenerateBinary.java
@@ -0,0 +1,25 @@
+package problems.queue;
+import java.util.*;
+public class GenerateBinary {
+
+
+        public static List<String> generateBinary(int n) {
+            List<String> ans = new ArrayList<>();
+            Queue<String> q = new LinkedList<>();
+            queue.offer("1");
+            for(int i=1; i <=n; i++) {
+                String cur=q.poll();
+                ans.add(cur);
+                q.offer(cur+"0");
+                q.offer(cur+"1");
+            }
+
+            return ans;
+        }
+
+        public static void main(String[] args) {
+            int n=5;
+            List<String> binaryList=generateBinary(n);
+            System.out.println(binaryList);
+        }
+    }
