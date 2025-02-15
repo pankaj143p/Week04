fe0124eff5c60dcd989594651a8fb0461fc4e304
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-15 11:41:38 +0530

    [Pankaj] : valid license plate number code added

--- day_05/src/main/java/problems/regex/ValidateLicensePlateNumber.java
+++ day_05/src/main/java/problems/regex/ValidateLicensePlateNumber.java
@@ -3,17 +3,17 @@ package problems.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ValidUserName {
+public class ValidateLicensePlateNumber {
     public static boolean isValidUserName(String userName){
-        String regex = "^[a-zA-Z][a-zA-Z0-9_]{4,15}$";
+        String regex = "^[A-Z]{2}[0-9]{4}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(userName);
         boolean flag=m.matches();
         return flag;
     }
     public static void main(String[] args) {
-        System.out.println(isValidUserName("User123_"));
-        System.out.println(isValidUserName("12User123"));
-        System.out.println(isValidUserName("Use"));
+        System.out.println(isValidUserName("AB1245"));
+        System.out.println(isValidUserName("A12332"));
+        System.out.println(isValidUserName("AB12383"));
     }
 }
