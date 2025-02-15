002124bbe8c77033e391997ceef5f55da4b92e33
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-15 12:41:27 +0530

    [Pankaj] : print all capital letter words code code added

--- day_05/src/main/java/problems/regex/PrintAllCapitalizeWords.java
+++ day_05/src/main/java/problems/regex/PrintAllCapitalizeWords.java
@@ -3,21 +3,23 @@ package problems.regex;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class PrintAllEmailAddress {
-    public static void possibleEmails(String text){
-        String regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}";
+
+public class PrintAllCapitalizeWords {
+    public static void possibleCapitalLetterWords(String text){
+        String regex = "\\b[A-Z][a-z]*\\b";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(text);
         while(m.find()){
-            System.out.println(m.group());
+            System.out.print(m.group()+", ");
         }
+        System.out.println();
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(1==1){
             System.out.println("Enter your text: ");
             String text=sc.nextLine();
-            possibleEmails(text);
+            possibleCapitalLetterWords(text);
             System.out.println("Enter y/n : y for exit n for further check :");
             char choice = sc.next().charAt(0);
             if(choice=='y'){
