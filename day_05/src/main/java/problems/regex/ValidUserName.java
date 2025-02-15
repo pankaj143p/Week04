cc79503a72ce6c4e791f1620c8335c2a561603ce
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-15 11:37:13 +0530

    [Pankaj] : username check code added

---
+++ day_05/src/main/java/problems/regex/ValidUserName.java
@@ -0,0 +1,19 @@
+package problems.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidUserName {
+    public static boolean isValidUserName(String userName){
+        String regex = "^[a-zA-Z][a-zA-Z0-9_]{4,15}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(userName);
+        boolean flag=m.matches();
+        return flag;
+    }
+    public static void main(String[] args) {
+        System.out.println(isValidUserName("User123_"));
+        System.out.println(isValidUserName("12User123"));
+        System.out.println(isValidUserName("Use"));
+    }
+}
