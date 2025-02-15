d9d70ce08e7ff7a4cb0dbe228ad83a675c2c27f2
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-15 14:50:44 +0530

    [Pankaj] : bad words replace code added

--- day_05/src/main/java/problems/regex/replaceandmodify/BadWords.java
+++ day_05/src/main/java/problems/regex/replaceandmodify/BadWords.java
@@ -1,22 +1,28 @@
 package problems.regex.replaceandmodify;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ReplaceMultipleSpace {
-    public static String removeMultipleSpaces(String text){
-        String regex = "\\s+",ans="";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(text);
-        return text.replaceAll(regex," ");
+public class BadWords {
+    private static List<String> badWords = Arrays.asList("dumb","stupid");
+
+    public static String removeBadWords(String text){
+        for(String bw : badWords){
+            String regex = "\\b"+Pattern.quote(bw)+"\\b";
+            text=text.replaceAll(regex,"****");
+        }
+        return text;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(1==1){
             System.out.println("Enter your text: ");
             String text=sc.nextLine();
-            System.out.println(removeMultipleSpaces(text));
+            System.out.println(removeBadWords(text));
             System.out.println("Enter y/n : y for exit n for further check :");
             char choice = sc.next().charAt(0);
             if(choice=='y'){
