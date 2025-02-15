135061b84270ff5e4d6355395f73ab9bce467f61
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-15 15:10:54 +0530

    [Pankaj] : valid credit card code added

--- day_05/src/main/java/problems/regex/advanced/ValidCreditCardNumber.java
+++ day_05/src/main/java/problems/regex/advanced/ValidCreditCardNumber.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ValidIpAddress {
-    public static boolean validIpAddress(String text){
-        String regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+public class ValidCreditCardNumber {
+    public static boolean validCardNumber(String text){
+        String regex = "^(4\\d{15}|5\\d{15})$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(text);
         boolean flag=m.matches();
@@ -16,8 +16,8 @@ public class ValidIpAddress {
         Scanner sc = new Scanner(System.in);
         while(1==1){
             System.out.println("Enter text");
-            String text=sc.nextLine().trim();
-            System.out.println(validIpAddress(text));
+            String number=sc.nextLine().trim();
+            System.out.println(validCardNumber(number));
             System.out.println("Enter y/n : y for exit n for further check :");
             char choice = sc.next().charAt(0);
             if(choice=='y'){
@@ -27,4 +27,3 @@ public class ValidIpAddress {
         }
     }
 }
-
